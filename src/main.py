d25f2f39263f518577a6fd611331ba95e7ccf835
Dehai Yu <dehai1@ualberta.ca>
2026-02-20 15:51:18 -0700

    thing

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ def main():
     print("Program that takes users skills and experience as input and outputs a\nregex string")
 
 
+    
     loop = True
     while loop:
 
@@ -20,6 +21,9 @@ def main():
         # experience: experience type, time started, time ended, additional comments
         
         print("options: 1. add skills\n 2. add experience\n")
+
+        user_skills = []
+        user_experience = []
         action = str(input("Action: "))
 
         if action == '1':
